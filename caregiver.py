f7f6c1444ce265320b87419124c5e7cdd3a9bbf2
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-05 15:50:45 -0500

    Update caregiver.py

--- caregiver.py
+++ caregiver.py
@@ -5,11 +5,20 @@ class Caregiver:
     """A class to represent a caregiver """
 
     def __init__(self, *, name='Caregiver', caregiver_id_num=0, caregiver_type=CaregiverType(5),
-                 min_hours=80, attributes=None, must_date=None, no_date=None):
+                 min_hours=80, attributes=None, must_dates=None, cant_dates=None):
         self.name = name
         self.caregiver_id_num = caregiver_id_num
         self.caregiver_type = CaregiverType(caregiver_type)
         self.min_hours = min_hours
-        self.attributes = attributes
-        self.must_date = must_date
-        self.no_date = no_date
+        if not attributes:
+            self.attributes = set()
+        else:
+            self.attributes = set(attributes)
+        if not must_dates:
+            must_dates = set()
+        else:
+            self.must_dates = set(must_dates)
+        if not cant_dates:
+            self.cant_dates = set()
+        else:
+            self.cant_date = set(cant_dates)
