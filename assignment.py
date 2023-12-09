d2b5172a6ab4adfde872067bc022eed248c35afe
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-08 19:17:08 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,8 @@
-class Assingment():
-    pass
+class Assignment():
+    """ Class accepting schedules, team, and evaluation objects,
+   which employs various strategy methods
+   for initial assignment and refinement methods """
+   
+   def __init__(self, rph_schedule, tech_schedule, team, evaluation):
+      pass
     
