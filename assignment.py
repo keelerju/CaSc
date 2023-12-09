d7fbea384aa41a22f394dc1e7f38cfd6e23d9e31
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-08 22:40:01 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,6 @@
+from random import choice
+
+
 class Assignment:
     """ Class with methods that accept schedules, team, and evaluation objects, which employs various strategy methods
     for initial assignment and refinement methods """
@@ -6,7 +9,9 @@ class Assignment:
         pass
 
     def initial_assignment(self, rph_schedule, tech_schedule, team, evaluation):
-        pass
+        for _caregiver_id_num in team.must_dates:
+            pass
+        random_shift = random.choice(rph_schedule)
 
     def refinement(self, rph_schedule, tech_schedule, team, evaluation):
         pass
