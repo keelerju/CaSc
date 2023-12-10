9ee5096e316812f783d454b8eac8974ed6b20bd0
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-10 09:33:12 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,7 +1,6 @@
 from datetime import date, datetime
 from random import choice
 from caregivertype import CaregiverType
-from copy import deepcopy
 
 
 class Assignment:
@@ -60,16 +59,19 @@ class Assignment:
                         chosen_shift = choice(_shifts)
                         tech_schedule[_shifts[0]].caregiver_id_num = chosen_shift[1].caregiver_id_num
 
-        # Create a copy of the RPh template.  If the RPh works a number of hours per pay period that when divided in
+        # Create local_team, a list of objects each with 2 attributes, the caregiver and the remaining hours
+        # yet to be assigned for them for that week. 
+        # If the RPh works a number of hours per pay period that when divided in
         # half is not divisible by the shift length (10 hours), then determine from a reference date in this method,
         # which week of the pay period is being assigned, then apportion the shifts for that caregiver so that the
         # first week, the remaining hours are rounded up to the nearest shift size, and the second week, the remaining
         # hours are rounded down to the nearest shift size.
+        # Loop through the team and assign the correct number of remaining hours to each tuple. 
         # Deduct the hours already assigned in that week of the RPh schedule from the remaining hours
         # Randomly select an RPh caregiver from then randomly select a shift, and if RPh has remaining hours,
         # and if there is no mismatch of skills, then assign the RPh to the shift.
 
-        local_rph_template = deepcopy(rph_template)
+        local_team = []
         reference_date_start_of_pay_period = datetime(2023, 12, 3)
         start_day = datetime(rph_schedule[0].year, rph_schedule[0].month, rph_schedule[0].day)
         week_difference = (start_day - reference_date_start_of_pay_period).days
