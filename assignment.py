1cf7c8911a3e198f00cf47ff36f06e7731f933c1
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-10 12:37:30 -0500

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -80,14 +80,13 @@ class Assignment:
                 pay_period_week = 2
         for week in range(rph_schedule[-1].week_of_month):
             assignable_team = []
-            for _assignable_caregiver in assignable_team:
-                remaining_hours = _assignable_caregiver.min_hours / 2
+            for _caregiver in team:
+                remaining_hours = _caregiver.min_hours / 2
                 if remaining_hours % 10 != 0:
                     if pay_period_week == 1:
                         remaining_hours += 5
                     elif pay_period_week == 2:
                         remaining_hours -= 5
-            for _caregiver in team:
                 assignable_team.append(AssignableCaregiver(_caregiver, remaining_hours))
             pay_period_week = 2 if pay_period_week == 1 else 1
 
