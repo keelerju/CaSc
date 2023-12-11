0a7d0faa44e09a6b6c684fdc8e95ca2f488017cc
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-11 08:14:52 -0500

    Update weekendrotation.py

--- weekendrotation.py
+++ weekendrotation.py
@@ -4,25 +4,28 @@ from datetime import date
 class WeekendRotation:
     """ Class containing the weekend rotations of all caregivers """
    
-   def __init__(self, rph_rotation=None, tech_rotation=None, reference_date=None):
-     if rph_rotation == None:
-        self.set_rph_rotation()
-     else:
-        self.rph_rotation = rph_rotation
-     if tech_rotation == None:
-        self.tech_rotation()
-     else:
-        self.tech_rotation = tech_rotation
-     if reference_date == None:
-        self.set_reference_date()
-     else:
-        self.reference_date = reference_date
+    def __init__(self, team, rph_rotation=None, tech_rotation=None, reference_date=None):
+     
+        self.team = team
+   
+        if rph_rotation == None:
+            self.set_rph_rotation()
+        else:
+            self.rph_rotation = rph_rotation
+        if tech_rotation == None:
+            self.tech_rotation()
+        else:
+            self.tech_rotation = tech_rotation
+        if reference_date == None:
+            self.set_reference_date()
+        else:
+            self.reference_date = reference_date
    
    def set_rph_rotation():
-      pass
+       pass
       
    def set_tech_rotation():
-      pass
+       pass
 
    def set_reference_date():
-      pass
+       pass
