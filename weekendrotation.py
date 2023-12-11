44f9bc72b6c9d294802beb70bf928d78cd9144f1
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-11 08:04:10 -0500

    Update weekendrotation.py

--- weekendrotation.py
+++ weekendrotation.py
@@ -1,3 +1,15 @@
 class WeekendRotation:
-    pass
-    
+    """ Class containing the weekend rotations of all caregivers """
+   
+   def __init__(self, rph_rotation, tech_rotation, reference_date):
+      self.rph_rotation = rph_rotation
+      self.tech_rotation = tech_rotation
+      self.reference_date = reference_date
+      self.set_rph_rotation()
+      self.set_tech_rotation()
+   
+   def set_rph_rotation():
+      pass
+      
+   def set_tech_rotation():
+      pass
