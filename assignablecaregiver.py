4cacc7b4449188312ac000d18261cc56ca137758
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-17 19:26:16 -0500

    Update assignablecaregiver.py

--- assignablecaregiver.py
+++ assignablecaregiver.py
@@ -1,5 +1,6 @@
 class AssignableCaregiver:
-    """ Class with attributes for the caregiver and the remaining hours of the week yet to be assigned """
+    """ Class with attributes for the caregiver and the remaining hours of the week yet to be assigned.
+        Similar to Team class, but use is temporary when assigning shifts.  """
 
     def __init__(self, caregiver, remaining_hours):
         self.caregiver = caregiver
