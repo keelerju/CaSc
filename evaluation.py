a25912712ac35624030300a56a3bf3f064a22c5d
Justin Keeler <121773324+keelerju@users.noreply.github.com>
2023-12-20 06:42:04 -0500

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -5,15 +5,18 @@ class Evaluation:
         self.rph_schedule = rph_schedule
         self.tech_schedule = tech_schedule
         self.team = team
+        self.evaluated_team = []
+        self.shift_locations_score = 0
+        self.max_shift_locations_score = 0
+        self.min_shift_locations_score = 0
+        self.shift_times_score = 0
+        self.max_times_score = 0
+        self.min_times_score = 0
 
     def evaluate(self):
         """ Evaluate each schedule for the various criteria and generate criteria scores and a composite score. """
 
         # Evaluate based on variety of shift locations per Caregiver
-        for shift in self.rph_schedule:
-            pass
-
-        # Evaluate based on number of shifts in either Inpatient or Retail pharmacy per Caregiver
         for shift in self.rph_schedule:
             pass
 
